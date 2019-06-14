9319209cc343e59e338328141c1f530c4ca29990
o.yesil <o.yesil-ext@osram.com>
2019-06-14 12:50:57 -0400

    added oauth2 cookie parser.

---
+++ Controllers/UtilityLib.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HttpHeaders_Utility.Controllers
+{
+    public class UtilityLib
+    {
+
+        public string EncodeString(string value)
+        {
+            return HttpUtility.HtmlDecode(value);
+        }
+    }
+}
